589cc76ba22342db802ef84c4eec5f13610dc6ad
Matty Squarzoni <mattysquarzoni@mac.attlocal.net>
2025-03-10 19:23:39 -0400

    Add ultra-simple debug script

--- debug.py
+++ debug.py
@@ -1,1 +1,6 @@
- 
+import streamlit as st
+import os
+st.title("Emergency Debug")
+st.write("Files in directory:", os.listdir("."))
+st.write("Data dir exists:", os.path.exists("data"))
+if os.path.exists("data"): st.write("Data files:", os.listdir("data"))
